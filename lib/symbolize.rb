4e45ce0d44870245e28695f66cf8ddbb00a555da
Marcos Piccinini <x@nofxx.com>
2013-02-19 04:03:30 -0300

    mongoid autoload working

--- lib/symbolize.rb
+++ lib/symbolize.rb
@@ -1,7 +1,7 @@
+# Todo: is this the best way for AR?
 module Symbolize
   autoload :ActiveRecord, 'symbolize/active_record'
-  autoload :Mongoid, 'symbolize/mongoid'
 end
 
-require 'symbolize/railtie' if defined? Rails
-
+require 'symbolize/mongoid' if defined? 'Mongoid'
+require 'symbolize/railtie' if defined? 'Rails'
--- lib/symbolize/version.rb
+++ lib/symbolize/version.rb
@@ -1,3 +1,3 @@
 module Symbolize
-  VERSION = '4.3.0'
+  VERSION = '4.3.1'
 end
