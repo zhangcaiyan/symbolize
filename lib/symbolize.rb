b7b59654c9cd532b89c33359dceefc30dc006129
ronen barzel <ronen@barzel.org>
2013-02-19 09:41:33 +0000

    bug fix: was always requiring both mongoid & rails
    the defined? guards were checking string literals rather than symbols, so were always returning true.  
    
    (for the case of Mongoid this meant that symbolize/mongoid was always being included, which as a side effect caused the symbol ::Mongoid to be defined, which then broke other gems which likewise checked for the existence of that symbol before doing mongo-specific things)

--- lib/symbolize.rb
+++ lib/symbolize.rb
@@ -3,5 +3,5 @@ module Symbolize
   autoload :ActiveRecord, 'symbolize/active_record'
 end
 
-require 'symbolize/mongoid' if defined? 'Mongoid'
-require 'symbolize/railtie' if defined? 'Rails'
+require 'symbolize/mongoid' if defined? Mongoid
+require 'symbolize/railtie' if defined? Rails
